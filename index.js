79ccb242f891cef59a9432d72e07961383a64f2e
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-11-14 15:13:23 -0600

    added console log

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ function calculateTotal(subtotal,shipping = 2.50) {
 };
 
 function printTopThreeHeadlines(...headlines) {
-  return headlines["headline 1""\n"] ["headline 2""\n"],["headline 3""\n"] ;
-};
+  return headlines.slice(0, 3).join('\n');
+
+console.log(calculateTotal(10));    
+console.log(
+  printTopThreeHeadlines(
+    "Headline One",
+    "Headline Two",
+    "Headline Three",
+    "Headline Four"
+  )
+);
   
 //should create a function named calculateTotal that takes two parameters subtotal and shipping and returns the sum of both values
 //should set a default value for shipping to 2.50
 //should create a function named printTopThreeHeadlines that takes a rest parameter list of string headlines named headlines and returns the top three headlines with a new line between each headline
+
 
 
 

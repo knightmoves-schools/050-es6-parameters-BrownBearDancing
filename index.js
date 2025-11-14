7e70ffd8ccaf300d63e65a78def4ec2ef4ec09c5
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-11-14 14:31:04 -0600

    not sure how to 3rd bullet

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 let calculateTotal = (subtotal,shipping = 2.50) => {
- return subtotal + shipping
+ return subtotal + shipping;
 };
 
 function printTopThreeHeadlines = (...headline) {
-  return headlines.slice(0, 3).join("\n");
+  return headlines[0 \n] + [1 \n] = [2 \n] ;
 };
   
 //should create a function named calculateTotal that takes two parameters subtotal and shipping and returns the sum of both values
 //should set a default value for shipping to 2.50
 //should create a function named printTopThreeHeadlines that takes a rest parameter list of string headlines named headlines and returns the top three headlines with a new line between each headline
+
 
 
 

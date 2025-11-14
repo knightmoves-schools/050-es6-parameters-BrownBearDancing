5bc157ef45a21a63b4b3cbf1a9442bca91d89a2a
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-11-14 14:17:44 -0600

    try this

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-let calculateTotal= (subtotal, shipping) => {
-  return subtotal + shipping;
+let calculateTotal= (a,b) => {
+  let subtotal = a;
+  let shipping =  b;
+  let b = 2.50
+  return a+ b;
 };
-let shipping = 2.50;
-let printTopThreeHeadlines = 
+
+let printTopThreeHeadlines = (...headline) => {
+  return headline[0], n/headline[1], n/headline[2];
+};
+  
 //should create a function named calculateTotal that takes two parameters subtotal and shipping and returns the sum of both values
 //should set a default value for shipping to 2.50
-should create a function named printTopThreeHeadlines that takes a rest parameter list of string headlines named headlines and returns the top three headlines with a new line between each headline
+//should create a function named printTopThreeHeadlines that takes a rest parameter list of string headlines named headlines and returns the top three headlines with a new line between each headline
+

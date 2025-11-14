44c3e1fd6c5d2ef81a3f3a9ae2ebdb820ea8c5ec
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-11-14 14:47:48 -0600

    i have no idea what i'm doing for line 6

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-let calculateTotal = (subtotal,shipping = 2.50) {
+let calculateTotal = (subtotal,shipping = 2.50) => {
  return subtotal + shipping;
 };
 
 function printTopThreeHeadlines = (...headlines) {
-  return headlines["headline 1"]["\n"], ["headline 2"] ["\n"],["headline 3"] ["\n"] ;
+  return headlines["headline 1""\n"] ["headline 2""\n"],["headline 3""\n"] ;
 };
   
 //should create a function named calculateTotal that takes two parameters subtotal and shipping and returns the sum of both values
 //should set a default value for shipping to 2.50
 //should create a function named printTopThreeHeadlines that takes a rest parameter list of string headlines named headlines and returns the top three headlines with a new line between each headline
+
 
 
 

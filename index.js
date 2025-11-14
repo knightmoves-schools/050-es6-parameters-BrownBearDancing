7fa5f94c5d2d07101298e438ce52908a2ea51430
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-11-14 14:24:26 -0600

    functions

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-let calculateTotal= (a,b) => {
-  let subtotal = a;
-  let shipping =  b;
-  let b = 2.50
-  return a+ b;
+let calculateTotal= (subtotal,shipping=2.50) => {
+ return subtotal + shipping
 };
 
-let printTopThreeHeadlines = (...headline) => {
-  return headline[0], n/headline[1], n/headline[2];
+function printTopThreeHeadlines = (...headline) {
+  return headlines.slice(0, 3).join("\n");
 };
   
 //should create a function named calculateTotal that takes two parameters subtotal and shipping and returns the sum of both values
 //should set a default value for shipping to 2.50
 //should create a function named printTopThreeHeadlines that takes a rest parameter list of string headlines named headlines and returns the top three headlines with a new line between each headline
+
 
